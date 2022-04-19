553a7f4e5a5d7157869677565c3e691f76b67d1d
samanthpulluru <50269313+samanthpulluru@users.noreply.github.com>
2022-04-19 21:15:55 +0530

    Node pool changes and more features invoked from module

--- dev/variables.tf
+++ dev/variables.tf
@@ -8,9 +8,20 @@ variable "cluster_name" {
   default     = "demo-project-gke-cluster"
 }
 
+variable "regional" {
+  description = "Boolean flag to determine regional presence of cluster"
+  default     = true
+  type        = boolean
+}
+
 variable "region" {
   description = "The region to host the cluster in"
   default     = "us-central1"
+}
+
+variable "zones" {
+  description = "list of zones the cluster has to be deployed across"
+  default     = ["us-central1-a", "us-central1-b", "us-central1-f"]
 }
 
 variable "network" {
@@ -33,8 +44,78 @@ variable "ip_range_services_name" {
   default     = "ip-range-scv"
 }
 
+variable "create_service_account" {
+  description = "Boolean flag to declare creation of a K8s service account"
+  default     = true
+}
+
+variable "horizontal_pod_scaling" {
+  description = "Boolean flag to implement auto pod scaling horizontally on the cluster"
+  default     = true
+}
+
 variable "gcp_credentials" {
   type        = string
   sensitive   = true
   description = "Google Cloud service account credentials"
+}
+
+variable "node_pool_name" {
+  description = "Name of the node pool associated with the cluster"
+  default     = "default-node-pool"
+}
+
+variable "machine_type" {
+  description = "Name of the machine type/size associated with the cluster"
+  default     = "e2-medium"
+}
+
+variable "node_locations" {
+  description = "list of locations the nodes can be deployed across"
+  default     = "us-central1-b,us-central1-c"
+}
+
+variable "min_count" {
+  description = "Minimum count of nodes the cluster can be operated on"
+  default     = 3
+}
+
+variable "max_count" {
+  description = "Maximum number of nodes the cluster can be scaled upto"
+  default     = 5
+}
+
+variable "disk_size" {
+  description = "Size of the disk associated with a node, in GB"
+  default     = 50
+}
+
+variable "disk_type" {
+  description = "Type of disk attached to a node"
+  default     = "pd-standard"
+}
+
+variable "image_type" {
+  description = "Type of image the nodes will be bootstrapped to"
+  default     = "COS_CONTAINERD"
+}
+
+variable "auto_repair" {
+  description = "Auto repair mode enabled for the nodes"
+  default     = true
+}
+
+variable "auto_upgrade" {
+  description = "Auto upgrade feature enabled for nodes/cluster"
+  default     = true
+}
+
+variable "node_init_count" {
+  description = "Cound of nods the cluster gets at the time of creation/restart"
+  default     = "e2-medium"
+}
+
+variable "terraform_service_account" {
+  description = "Mail ID of the service account associated with the Terraform deployment"
+  default     = "demo-project-terraform-service@ops-hiring-case-study-pulluru.iam.gserviceaccount.com"
 }
