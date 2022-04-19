0ce5d30cee84aea22a7eae578d1ff97c07d96a14
samanthpulluru <50269313+samanthpulluru@users.noreply.github.com>
2022-04-19 21:02:16 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -61,14 +61,33 @@ module "gcp-network" {
 }
 
 module "gke" {
-  source                 = "terraform-google-modules/kubernetes-engine/google"
-  project_id             = var.project_id
-  name                   = var.cluster_name
-  regional               = true
-  region                 = var.region
-  network                = module.gcp-network.network_name
-  subnetwork             = module.gcp-network.subnets_names[0]
-  ip_range_pods          = var.ip_range_pods_name
-  ip_range_services      = var.ip_range_services_name
-  create_service_account = true
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = var.project_id
+  name                       = var.cluster_name
+  regional                   = var.region_flag
+  region                     = var.region
+  zones                      = var.zones
+  network                    = module.gcp-network.network_name
+  subnetwork                 = module.gcp-network.subnets_names[0]
+  ip_range_pods              = var.ip_range_pods_name
+  ip_range_services          = var.ip_range_services_name
+  create_service_account     = var.create_service_account
+  horizontal_pod_autoscaling = var.horizontal_pod_autoscaling
+    
+  node_pools = [
+    {
+      name               = var.node_pool_name
+      machine_type       = var.machine_type
+      node_locations     = var.node_locations
+      min_count          = var.min_count
+      max_count          = var.max_count
+      disk_size_gb       = var.disk_size
+      disk_type          = var.disk_type
+      image_type         = var.image_type
+      auto_repair        = var.auto_repair
+      auto_upgrade       = var.auto_upgrade
+      service_account    = var.terraform_service_account
+      initial_node_count = var.node_init_count
+    },
+  ]
 }
